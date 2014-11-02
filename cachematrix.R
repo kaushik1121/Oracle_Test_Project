67b1945691e91d43a493ecfe3cc5747606deed26
kaushik1121 <kaushik.1121@gmail.com>
2014-11-02 14:29:13 -0800

    Update cachematrix.R
    Submitting the code for Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below two Functions will cache the inverse of a matrix
 
-## Write a short comment describing this function
+## Creating a matrix which will cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function( m = matrix() ) {
+
+    ## Initializing the inverse property
+    i <- NULL
+
+    ## Set the matrix
+    set <- function( matrix ) {
+	m <<- matrix
+	i <<- NULL
+    }
+
+    ## Get the matrix
+    get <- function() {
+    	## Returning the matrix
+    	m
+    }
+
+    ## Set the inverse of the matrix
+    setInverse <- function(inverse) {
+        i <<- inverse
+    }
+
+    ## Get the inverse of the matrix
+    getInverse <- function() {
+        ## Returning the inverse property
+        i
+    }
+
+    ## Returning a list of the methods
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 
 }
 
-
-## Write a short comment describing this function
+## Computing the inverse of the matrix returned by "makeCacheMatrix" above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the "cachesolve" will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    ## Returning a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+
+    ## Returning the inverse if its already set
+    if( !is.null(m) ) {
+	message("getting cached data")
+	return(m)
+    }
+
+    ## Getting the matrix from our object
+    data <- x$get()
+
+    ## Calculating the inverse using matrix multiplication
+    m <- solve(data) %*% data
+
+    ## Setting the inverse to the object
+    x$setInverse(m)
+
+    ## Returning the matrix
+    m
+
 }
